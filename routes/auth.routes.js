b5b100fee367a38dea759950c710aad2027571f1
cdelgado.es@gmail.com <cdelgado@acceso.com>
2023-08-13 13:03:04 +0200

    prueba commit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -47,7 +47,7 @@ router.post('/login', (req, res, next) => {
 })
 
 router.post('/logout', (req, res, next) => {
-	req.session.destroy(() => res.redirect('/login'))
+	// req.session.destroy(() => res.redirect('/login'))
 })
 
 module.exports = router
